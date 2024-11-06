fa47afd04d6093b3247f58bc449ba02dded07c33
findaditya <findadityaagrawal@gmail.com>
2024-11-06 21:00:53 +0530

    added pages and components dirs

---
+++ netflix/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import './Navbar.css'
+import logo from '../../assets/logo.png'
+import search_icon from '../../assets/search_icon.svg'
+import bell_icon from '../../assets/bell_icon.svg'
+
+
+const Navbar = () => {
+  return (
+    <div className='navbar'>
+      <div className="navbar-left">
+        <img src={logo} alt="" />
+        <ul>
+            <li>Home</li>
+            <li>TV Shows</li>
+            <li>Movies</li>
+            <li>New & Popular</li>
+            <li>My List</li>
+            <li>Browse by languages</li>
+        </ul>
+      </div>
+      <div className="navbar-right">
+      <img src={search_icon} alt="" className='icons'/>
+      <img src={bell_icon} alt="" className='icons'/>
+      </div>
+    </div>
+  )
+}
+
+export default Navbar
